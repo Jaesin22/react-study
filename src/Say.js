b1016753a7eb965075dd1a50267c9e98170c5d49
Jaesin22 <jaesin22@naver.com>
2022-01-01 17:04:17 +0900

    [0101] Page.114 useState 2

--- src/Say.js
+++ src/Say.js
@@ -5,12 +5,16 @@ const Say = () => {
     const onClickEnter = () => setMessage('hi hi')
     const onClickLeave = () => setMessage('bye bye')
 
+    const [color, setColor] = useState('black')
+
     return (
         <div>
             <button onClick ={onClickEnter}>입장</button>
             <button onClick ={onClickLeave}>퇴장</button>
-            <h1>{message}</h1>
-
+            <h1 style={{color}}>{message}</h1>
+             <button style={{color: 'red'}} onClick={() => setColor('red')}>빨간색</button>
+             <button style={{color: 'green'}} onClick={() => setColor('green')}>빨간색</button>
+             <button style={{color: 'blue'}} onClick={() => setColor('blue')}>빨간색</button>
         </div>
     )
 
